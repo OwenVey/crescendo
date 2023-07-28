347ed92a04ee24839d9cb05bb47c76acea09caf1
Owen Vey <owen.vey@gmail.com>
2023-07-28 12:00:37 -0500

    make default color gray but map to neutral

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -15,6 +16,9 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['var(--font-inter)', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        gray: colors.neutral,
       },
       keyframes: {
         'accordion-down': {
