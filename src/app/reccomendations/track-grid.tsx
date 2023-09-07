db80cc707fe267a5841041208c400826a07b64f0
Owen Vey <owen.vey@gmail.com>
2023-09-07 14:56:17 -0500

    change grids back to flex-wrap

--- src/app/reccomendations/track-grid.tsx
+++ src/app/reccomendations/track-grid.tsx
@@ -116,8 +116,8 @@ export async function TrackGrid({ searchParams }: TrackGridProps) {
   }
 
   return (
-    // <div className="flex flex-wrap gap-8 overflow-y-auto p-8">
-    <div className="grid grid-cols-[repeat(auto-fill,var(--card-width))] justify-between gap-8 overflow-y-auto p-8">
+    // <div className="grid grid-cols-[repeat(auto-fill,var(--card-width))] justify-between gap-8 overflow-y-auto p-8">
+    <div className="flex flex-wrap gap-8 overflow-y-auto p-8">
       {recommendations.tracks.map((track, index) => (
         <TrackCard key={track.id} track={track} index={index} />
       ))}
