590168c3e9a95e710a079864372f81adca959802
Owen Vey <owen.vey@gmail.com>
2023-09-11 17:28:01 -0500

    change grid view to use grid over flex

--- src/app/reccomendations/grid/grid-view-loading.tsx
+++ src/app/reccomendations/grid/grid-view-loading.tsx
@@ -2,8 +2,8 @@ import { Skeleton } from '@/components/ui/skeleton';
 
 export function GridViewLoading() {
   return (
-    // <div className="grid grid-cols-[repeat(auto-fill,var(--card-width))] justify-between gap-8 overflow-y-auto p-8">
-    <div className="flex flex-wrap gap-8 overflow-y-auto p-8">
+    // <div className="flex flex-wrap gap-8 overflow-y-auto p-8">
+    <div className="grid grid-cols-[repeat(auto-fill,var(--card-width))] justify-around gap-8 overflow-y-auto p-8">
       {[...Array(100)].map((_, index) => (
         <GridTrackItemSkeleton key={index} />
       ))}
@@ -15,7 +15,7 @@ function GridTrackItemSkeleton() {
   return (
     <div className="w-[--card-width]">
       <Skeleton className="aspect-square w-full rounded-2xl" />
-      <Skeleton className="mt-2 h-5 w-full" />
+      <Skeleton className="mt-2 h-5 w-1/2" />
       <Skeleton className="mt-1 h-4 w-3/4" />
     </div>
   );
--- src/app/reccomendations/grid/grid-view.tsx
+++ src/app/reccomendations/grid/grid-view.tsx
@@ -8,8 +8,8 @@ export function GridView() {
   const reccomendations = useAtomValue(reccomendationsAtom);
 
   return (
-    // <div className="grid grid-cols-[repeat(auto-fill,var(--card-width))] justify-between gap-8 overflow-y-auto p-8">
-    <div className="flex flex-wrap gap-8 overflow-y-auto p-8">
+    // <div className="flex flex-wrap gap-8 overflow-y-auto p-8">
+    <div className="grid grid-cols-[repeat(auto-fill,var(--card-width))] justify-around gap-8 overflow-y-auto p-8">
       {reccomendations.map((track, index) => (
         <GridTrackItem key={track.id} track={track} index={index} />
       ))}
